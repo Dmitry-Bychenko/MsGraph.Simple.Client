4f128e6831f5ce47f84d1f024615990deb4ed56b
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-09-22 00:12:24 +0300

    develop / 1.0.0.0 icon

--- MsGraph.Simple.Client/Graph/MsGraph.Simple.Client.Graph.UserExtensions.cs
+++ MsGraph.Simple.Client/Graph/MsGraph.Simple.Client.Graph.UserExtensions.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.Graph;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace MsGraph.Simple.Client.Graph {
 
